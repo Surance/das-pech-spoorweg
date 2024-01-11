d02e1b5e9a9eac76781414a7a67593b3dd989868
Surance <jmtbbn@gmail.com>
2024-01-11 13:38:19 +0100

    connection class aangemaakt

---
+++ connection.py
@@ -0,0 +1,5 @@
+class Connection:
+    def __init__(self, departure_station, arrival_station, travel_time):
+        self.departure_station = departure_station
+        self.arrival_station = arrival_station
+        self.travel_time = travel_time
