5d878bc1f94ad1ae9f69ec28ef5c3fa963893f6c
josephinewoltering <j.woltering123@gmail.com>
2024-01-11 13:55:26 +0100

    make traject class

---
+++ trajectMain.py
@@ -0,0 +1,6 @@
+class Traject():
+    def __init__(self):
+        self.stations_list = []
+
+    def traject_stations(self):
+        self.stations_list.append(Station(name, x_coordinate, y_coordinate))
