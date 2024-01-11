5f5ebe78b300d5b9ed925bdb4fdf88d93c160792
josephinewoltering <j.woltering123@gmail.com>
2024-01-11 15:03:01 +0100

    add time and connections to traject

--- trajectMain.py
+++ trajectMain.py
@@ -1,6 +1,20 @@
+import station 
+import connection
+
 class Traject():
-    def __init__(self):
-        self.stations_list = []
+    def __init__(self, traject_name):
+        self.stations_names_list = []
+        self.connections_list = []
+        self.traject_name = traject_name
+        self.total_time = 0
 
     def traject_stations(self):
-        self.stations_list.append(Station(name, x_coordinate, y_coordinate))
+        """List of train stations the traject passes to use for output"""
+        self.stations_names_list.append(station.Station.name)
+    
+    def connections_traject(self):
+        self.connections_list.append(connection.Connection())
+
+    def track_traject_time(self):
+        for connection in self.connections_list:
+            self.total_time += connection.time
