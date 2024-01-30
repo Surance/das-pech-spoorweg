54e28346fdb0edad5701247818c266cb13598232
josephinewoltering <j.woltering123@gmail.com>
2024-01-30 18:59:46 +0100

    show dataframe for specific output

--- code/visualisation/comparison.py
+++ code/visualisation/comparison.py
@@ -1,3 +1,5 @@
+import csv 
+import pandas as pd
 # import matplotlib.pyplot as plt
 
 
@@ -22,3 +24,9 @@
 # plt.legend(loc='upper right')
 
 # plt.show()
+random_data_df = pd.read_csv("experiment/random/random_7562/EXPERIMENT_SUMMARY")
+hillclimb_data_df = pd.read_csv("experiment/hillclimb_train/hillclimb_train_7/EXPERIMENT_SUMMARY")
+
+print(random_data_df)
+
+print(hillclimb_data_df)
